e112262513a465f253a9c3e938b7f6c1194b2ce3
Little Blue <blue@littles-air.lan>
2020-07-08 11:57:41 +0800

    create cube class

--- snake_game.py
+++ snake_game.py
@@ -4,8 +4,35 @@ import random
 import tkinter
 from tkinter import messagebox
 
-width = 500
-height = 500
+width, height = 500, 500
 
-cols = 25
-rows = 20
+cols, rows = 20, 25
+
+
+class cube(object):
+
+    def __init__(self, start, dirnx=1, dirny=0, color=(255,0,0)):
+        super().__init__()
+        self.pos = start
+        self.dirnx = dirnx
+        self.dirny = dirny
+        self.color = color
+
+    def move(self, dirnx, dirny):
+        self.dirnx = dirnx
+        self.dirny = dirny
+        self.pos  = (self.pos[0] + self.dirnx, self.pos[1] + self.dirny)
+
+    def draw(self, surface, eyes=False):
+        dis = self.w // self.rows
+        i = self.pos[0]
+        j = self.pos[1]
+        
+        pygame.draw.rect(surface, self.color, (i*dis+1,j*dis+1,dis-2,dis-2))
+        if eyes:
+            centre = dis//2
+            radius = 3
+            circleMiddle = (i*dis+centre-radius,j*dis+8)
+            circleMiddle2 = (i*dis + dis -radius*2, j*dis+8)
+            pygame.draw.circle(surface, (0,0,0), circleMiddle, radius)
+            pygame.draw.circle(surface, (0,0,0), circleMiddle2, radius)
